21f6d62b9ba55e80c4473935c1abd8e4df7ecda6
ianmelo1 <ianmelo171@gmail.com>
2025-07-01 22:57:41 -0300

    Atualizando segurança e excessões

--- Calculadora Tinta.py/comodo.py
+++ Calculadora Tinta.py/comodo.py
@@ -1,9 +1,37 @@
 class Comodo:
-    largura: float
-    profundidade: float
-    altura: float
+    __largura: float
+    __profundidade: float
+    __altura: float
 
     def __init__(self, largura, profundidade):
-        self.largura = float(largura)
-        self.profundidade = float(profundidade)
-        self.altura = 2.9
+        self.largura = largura
+        self.profundidade = profundidade
+        self.__altura = 2.9
+
+    @property
+    def largura(self):
+        return self.__largura
+
+    @property
+    def profundidade(self):
+        return self.__profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @largura.setter
+    def largura(self, largura):
+        try:
+            self.__largura = float(largura)
+        except Exception:
+            print('Largura inválida')
+            exit()
+
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        try:
+            self.__profundidade = float(profundidade)
+        except Exception:
+            print('Profundidade inválida')
+            exit()
